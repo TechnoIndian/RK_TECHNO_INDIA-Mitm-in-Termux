2cfc780a612aa5f1bfb88b76a5e4c260a6f808a7
TechnoIndian <133761719+TechnoIndian@users.noreply.github.com>
2024-05-12 00:14:34 +0530

    fsu.sh

--- fsu.sh
+++ fsu.sh
@@ -1,10 +1,1 @@
-# update and upgrade termux packages 
-pkg update -y && pkg upgrade -y
-
-# install proot-distro to install ubuntu
-pkg install proot-distro -y
-
-# install ubuntu
-proot-distro install ubuntu
-
-echo "proot-distro login ubuntu" > $HOME/../usr/bin/fsu && chmod +x $HOME/../usr/bin/fsu
+pkg update -y && pkg upgrade -y && pkg install proot-distro -y && proot-distro install ubuntu && echo "proot-distro login ubuntu" > $HOME/../usr/bin/fsu && chmod +x $HOME/../usr/bin/fsu && fsu && apt update -y && apt upgrade -y
